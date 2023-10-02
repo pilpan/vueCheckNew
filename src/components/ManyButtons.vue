d638f2a1d7cd1f22e1bb7df9db2bf64ef84ea0e8
plip <plip2010195@yandex.ru>
2023-10-02 16:20:52 +0300

    add gitCommand

--- src/components/ManyButtons.vue
+++ src/components/ManyButtons.vue
@@ -1,54 +1,31 @@
-<template lang="">
-    <div class='center'>
-        <h1 @click="say('Нажми на тык',$event)">Приветствие на разных языках</h1>
-        <div class="Manybutton">
-            <div v-for="(el, index) in list">
-                <button @click="sayHi" :id="index">Тык</button>
-            </div>
-        </div>
+<template>
+    <div class="redBox">
     </div>
+    <p>hello world</p>
 </template>
 <script>
-import { useRoute } from 'vue-router'
-
 export default {
     data() {
         return {
-            section: useRoute(),
-            list: [
-                { text: '안녕하세요' },
-                { text: 'Hola' },
-                { text: 'Ciao' },
-                { text: 'Hallo' },
-                { text: 'Привет' },
-            ]
+            redBox: {
+                width: "40px",
+                height: "40px",
+                backgroundColor: "red",
+            },
+            active:false,
+            greenBox: {
+                width: "40px",
+                height: "40px",
+                backgroundColor: "green",
+            }
         }
     },
-    methods: {
-        sayHi(event) {
-            console.log(event.target.id)
-            alert(this.list[event.target.id].text)
-
-        },
-        say(data,event){
-            console.log(event)
-            alert(data)
-        }
-    },
-    mounted() {
-    console.log(this.section.name)
-  },
 }
 </script>
 <style>
-.Manybutton {
-    display: inline-Flex;
-    width: 100%;
-}
-.center{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+.redBox {
+    width: 40px;
+    height: 40px;
+    background-color: red;
 }
 </style>
